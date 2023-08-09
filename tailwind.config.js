c8cf31d3760055a7224b6291e5dc8bfce4daf1d8
Javier Granados <hola@javiergranados.dev>
2023-08-09 13:24:24 +0200

    Create light and dark themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: '#051221',
+          secondary: '#80B2ED',
+          accent: '#8F3985',
+          neutral: '#ffff',
+          'base-100': '#C9DFF7',
+          info: '#80B2ED',
+          success: '#94C9A9',
+          warning: '#F87575',
+          error: '#F87575',
+        },
+        dark: {
+          primary: '#80B2ED',
+          secondary: '#ffff',
+          accent: '#8F3985',
+          neutral: '#ffff',
+          'base-100': '#051221',
+          info: '#80B2ED',
+          success: '#94C9A9',
+          warning: '#F87575',
+          error: '#F87575',
+        },
+      },
+    ],
+  },
   plugins: [require('daisyui')],
 }
