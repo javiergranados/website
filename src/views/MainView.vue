e19a507fde1280ea3f47541da468e76b60c4e0ad
Javier Granados <hola@javiergranados.dev>
2023-09-16 18:35:36 +0200

    Implement main view

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts">
-const msg = 'Main View'
-</script>
-
 <template>
-  <h1 class="text-4xl text-primary">{{ msg }}</h1>
+  <h1 class="text-4xl font-semibold text-primary">
+    Hi everyone!&nbsp;&nbsp;
+    <span role="img" aria-label="smiley face"> 👋🏻🙂 </span>
+  </h1>
+  <p class="text-3xl text-primary">
+    I&apos;m Javier Granados, a frontend developer based in Seville, Spain&nbsp;
+    <span role="img" aria-label="flag"> 🇪🇸 </span>
+  </p>
 </template>
