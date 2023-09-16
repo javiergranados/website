8bd05064078477cd3757b73e42edb30b5ce29cb7
Javier Granados <hola@javiergranados.dev>
2023-09-16 22:46:53 +0200

    Rename MainView component by HomeView

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import './style.css'
 import About from './views/AboutView.vue'
-import MainView from './views/MainView.vue'
+import Home from './views/HomeView.vue'
 import Projects from './views/ProjectsView.vue'
 import Uses from './views/UsesView.vue'
 
 const routes = [
-  { path: '/', name: 'main', component: MainView },
+  { path: '/', name: 'home', component: Home },
   { path: '/projects', name: 'projects', component: Projects },
   { path: '/uses', name: 'uses', component: Uses },
   { path: '/about', name: 'about', component: About },
