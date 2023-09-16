ac5b253cbeccd028a908fffc86763fa7bc926dd9
Javier Granados <hola@javiergranados.dev>
2023-09-16 23:12:48 +0200

    Add a navigation guard to modify the document title on each navigation

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,49 @@ import Projects from './views/ProjectsView.vue'
 import Uses from './views/UsesView.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/projects', name: 'projects', component: Projects },
-  { path: '/uses', name: 'uses', component: Uses },
-  { path: '/about', name: 'about', component: About },
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    meta: {
+      title: 'Home - Javier Granados',
+    },
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: Projects,
+    meta: {
+      title: 'Projects | Javier Granados',
+    },
+  },
+  {
+    path: '/uses',
+    name: 'uses',
+    component: Uses,
+    meta: {
+      title: 'Uses | Javier Granados',
+    },
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About,
+    meta: {
+      title: 'About | Javier Granados',
+    },
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.afterEach((to, _from, failure) => {
+  if (!failure) {
+    document.title = to.meta.title as string
+  }
 })
 
 createApp(App).use(router).mount('#app')
